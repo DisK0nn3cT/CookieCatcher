4982bbaedca5b08be6f78b88035e7d719e1b4b4f
DisK0nn3cT <danny.chrastil@gmail.com>
2013-08-21 04:52:09 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -95,7 +95,6 @@ $payload = htmlspecialchars($payload);
 <html>
 <head>
 <title>CookieCatcher BETA v0.1</title>
-<<<<<<< HEAD
 <script>
 function c() {
   p = document.getElementById('payloadcode');
@@ -152,46 +151,6 @@ X8888  X888h        888R Y888r  888R Y888r   888E u@8NL   .@88u    ud8888.     X
 <p><?php echo $message; ?></p>
 
 <div class="table"><?php echo $message=='' ? $cookieResults : '<a class="button" href="?">back</a><a class="button" href="?">delete</a>'; ?></p></div>
-=======
-<style>
-h1,h2,h3 { color:#333; font-family: 'Crushed', cursive; }
-body {
-background: #cedce7!important;
-background: -moz-linear-gradient(top,  #cedce7 0%, #596a72 100%);
-background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#cedce7), color-stop(100%,#596a72));
-background: -webkit-linear-gradient(top,  #cedce7 0%,#596a72 100%);
-background: -o-linear-gradient(top,  #cedce7 0%,#596a72 100%);
-background: -ms-linear-gradient(top,  #cedce7 0%,#596a72 100%);
-background: linear-gradient(top,  #cedce7 0%,#596a72 100%);
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#cedce7', endColorstr='#596a72',GradientType=0 );
-height:1500px;
-}
-#payload { padding:10px; background:#ffffff; border:1px dashed #666666; text-align:center; font-family:courier; color:#666 }
-#payload2 { padding:10px; background:#ffffff; border:1px dashed #666666; text-align:left; font-family:courier; color:#666 }
-span { font-family: courier; font-size:12px; }
-#tag { padding:2px; background:#666; color:#ffffff; font-weight: normal; font-size:12px; text-align:center; width:80px;}
-#frame { width:100%; height:400px; border:1px dashed #666666; }
-.table { background: #ffffff; border:1px dashed #666666; opacity:0.5;}
-.column { float:left; padding:5px 15px; min-width:5%; background-color:#fff; margin-bottom:2px;}
-.right { float:right };
-</style>
-</head>
-<body>
-
-<PRE>  ____            _    _         ____      _       _               
- / ___|___   ___ | | _(_) ___   / ___|__ _| |_ ___| |__   ___ _ __ 
-| |   / _ \ / _ \| |/ / |/ _ \ | |   / _` | __/ __| '_ \ / _ \ '__|
-| |__| (_) | (_) |   &lt;| |  __/ | |__| (_| | || (__| | | |  __/ |   
- \____\___/ \___/|_|\_\_|\___|  \____\__,_|\__\___|_| |_|\___|_|   
-                                                                   
-</PRE>
-<div id="tag">XSS Payload</div>
-<div id="payload"><?php echo $payload;?></div>
-
-<p><?php echo $message; ?></p>
-<p>
-<div class="table"><?php echo $message=='' ? $cookieResults : '<a href="?">back</a>'; ?></p></div>
->>>>>>> a765228d6b9b129e5db14207d006a4689f83af13
 
 </body>
 </html>
