95b2421be192ff5d385ee6356ed603e48404cb47
DisK0nn3cT <danny.chrastil@gmail.com>
2013-08-21 02:08:13 +0000

    Updated SETUP.sql

--- SETUP.sql
+++ SETUP.sql
@@ -13,13 +13,16 @@ CREATE TABLE `cookies` (
   `cookiedata` text,
   `ip` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=130 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `cookies`
+-- Table structure for table `payloads`
 --
 
-LOCK TABLES `cookies` WRITE;
-INSERT INTO `cookies` VALUES (1,'http://www.google.com','testcookie=testdata','192.168.59.130'),(2,'http://www.owasp.org','testcookie=testdata','192.168.59.16'),(4,'http://www.owasp.org','testcookie=testdata','192.168.59.26'),(5,'http://www.owasp.org','testcookie=testdata','192.168.59.130'),(13,'http://banksite.com/','testing','192.168.59.1');
-UNLOCK TABLES;
-
+DROP TABLE IF EXISTS `payloads`;
+CREATE TABLE `payloads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `payload` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
